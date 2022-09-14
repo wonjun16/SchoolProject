8a29ba90644d6685654b7aaa9fcd00e8d9cc8e82
wonjun16 <jun9816@naver.com>
2022-09-15 02:03:38 +0900

    DistinctSubsequence ver_2

--- AlgorithmClass/DistinctSubsequence.c
+++ AlgorithmClass/DistinctSubsequence.c
@@ -1,16 +1,30 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include<stdio.h>
 #include<string.h>
+#define SIZEF 101
+#define SIZEO 10001
 
-int memo[100][10000];
+int memo[SIZEF][SIZEO] = { 0 };
 
-void distinctSubsequence(char oWord[], char fWord[]) {
+int distinctSubsequence(char oWord[], char fWord[]) {
 	int oLength = strlen(oWord), fLength=strlen(fWord);
-	for (int i = 0; i < fLength; i++) {
-		for (int j = 0; j < oLength; j++) {
-
+	for (int i = 1; i < fLength+1; i++) {
+		for (int j = 1; j < oLength+1; j++) {
+			if (oWord[j - 1] == fWord[i - 1] && i == 1)
+				memo[i][j]++;
+			else if (oWord[j - 1] == fWord[i - 1])
+				memo[i][j] = memo[i - 1][j] + memo[i][j - 1];
+			else
+				memo[i][j] = memo[i][j - 1];
 		}
 	}
+	for (int i = 1; i < fLength + 1; i++) {
+		for (int j = 1; j < oLength + 1; j++) {
+			printf("%d", memo[i][j]);
+		}
+		printf("\n");
+	}
+	return memo[fLength][oLength];
 }
 
 void main() {
@@ -18,10 +32,11 @@ void main() {
 	printf("input number of test cases : ");
 	scanf("%d", &n);
 	for (int i = 0; i < n; i++) {
-		char originalWord[10000], findWord[100];
+		char originalWord[SIZEO], findWord[SIZEF];
 		printf("Input original word : ");
 		scanf("%s", originalWord);
 		printf("Input find word : ");
 		scanf("%s", findWord);
+		printf("find word in original word : %d\n", distinctSubsequence(originalWord, findWord));
 	}
 }
